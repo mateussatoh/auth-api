38f9d8cd35b3b177a3db5c0dcb04b44cbadad5d7
Mateus Satoh <mateussatoh@gmail.com>
2021-03-29 18:51:10 -0300

    add: end of tutorial

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import AuthMiddleware from "./app/middlewares/AuthMiddleware";
+import AuthController from "./app/controllers/AuthController";
+import UserController from "./app/controllers/UserController";
+
+const router = Router();
+
+router.post("/users", UserController.store);
+router.post("/auth", AuthController.authenticate);
+router.get("/users", AuthMiddleware, UserController.index);
+
+export default router;
